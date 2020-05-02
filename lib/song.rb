69358b33a7f35ad746dd2fe767c93b918d6394f5
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:48:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,57 @@
-class Song
-  attr_accessor :name, :artist
+#
+#
+# class Song
+#   attr_accessor :name, :artist
+#   @@all = []
+#   def initialize(name, artist=nil)
+#     @name = name
+#     @artist = artist
+#     @@all << self
+#   end
+#
+#   def artist_name=(name)
+#     if (self.artist.nil?)
+#       self.artist = Artist.new(name)
+#     else
+#       self.artist.name = name
+#     end
+#   end
+#
+#   def self.new_by_filename(filename)
+#     song = self.new()
+#     song.title = filename.split(" - ")[1]
+#     song
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+# end
 
+class Song
+
+  attr_accessor :name, :artist
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def self.new_by_filename(filename)
-    artist_name, song_name, extra = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
+      artist_name, song_name, extra = filename.split(" - ")
+      song = self.new(song_name)
+      artist = Artist.find_or_create_by_name(artist_name)
+      artist.add_song(song)
+    end
+
+  def self.all
+    @@all
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
 end
