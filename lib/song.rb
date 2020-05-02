3aca9fb006d373e50d512debb9d1cf8faa4ea301
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:53:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,4 @@
-#
-#
-# class Song
-#   attr_accessor :name, :artist
-#   @@all = []
-#   def initialize(name, artist=nil)
-#     @name = name
-#     @artist = artist
-#     @@all << self
-#   end
-#
-#   def artist_name=(name)
-#     if (self.artist.nil?)
-#       self.artist = Artist.new(name)
-#     else
-#       self.artist.name = name
-#     end
-#   end
-#
-#   def self.new_by_filename(filename)
-#     song = self.new()
-#     song.title = filename.split(" - ")[1]
-#     song
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-# end
+
 
 class Song
 
