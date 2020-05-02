e4b863a659943453e7162f3aa8617828825f60ee
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:38:10 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,23 @@
+# class MP3Importer
+#   def import(list_of_filenames)
+#     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+#   end
+# end
+
 class MP3Importer
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
   end
+
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
+
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
+
 end
