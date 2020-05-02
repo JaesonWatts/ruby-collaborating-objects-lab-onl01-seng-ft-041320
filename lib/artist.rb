68377b296e76e7e9f5d5ec32b3e5be35297829a9
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:45:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,6 +8,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
@@ -15,11 +16,11 @@ class Artist
   end
 
   def save
-    @@all << self #saves the artist name to the all array
+    @@all << self 
   end
 
   def self.all
-    @@all #displays the all array
+    @@all 
   end
 
   def self.find_or_create_by_name(artist_name)
