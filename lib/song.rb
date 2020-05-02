2855fc38577a182d2aa1d994f4b52a3ac449a698
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:44:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,53 +1,53 @@
-#
-#
-# class Song
-#   attr_accessor :name, :artist
-#   @@all = []
-#   def initialize(name, artist=nil)
-#     @name = name
-#     @artist = artist
-#     @@all << self
-#   end
-#
-#   def artist_name=(name)
-#     if (self.artist.nil?)
-#       self.artist = Artist.new(name)
-#     else
-#       self.artist.name = name
-#     end
-#   end
-#
-#   def self.new_by_filename(filename)
-#     song = self.new()
-#     song.title = filename.split(" - ")[1]
-#     song
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-# end
+
 
 class Song
-
   attr_accessor :name, :artist
-
-  def initialize(name)
+  @@all = []
+  def initialize(name, artist=nil)
     @name = name
-  end
-
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    @artist = artist
+    @@all << self
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
+  def self.new_by_filename(filename)
+    song = self.new()
+    song.title = filename.split(" - ")[1]
+    song
+  end
+
+  def self.all
+    @@all
   end
 
 end
+
+# class Song
+#
+#   attr_accessor :name, :artist
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def self.new_by_filename(file_name)
+#     song = file_name.split(" - ")[1]
+#     artist = file_name.split(" - ")[0]
+#     new_song = self.new(song)
+#     new_song.artist_name = artist
+#     new_song
+#   end
+#
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     artist.add_song(self)
+#   end
+#
+# end
