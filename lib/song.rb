3d6dd2a54eb7cc5a32a6f389830c2438a7d753ed
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:40:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,53 @@
-
+#
+#
+# class Song
+#   attr_accessor :name, :artist
+#   @@all = []
+#   def initialize(name, artist=nil)
+#     @name = name
+#     @artist = artist
+#     @@all << self
+#   end
+#
+#   def artist_name=(name)
+#     if (self.artist.nil?)
+#       self.artist = Artist.new(name)
+#     else
+#       self.artist.name = name
+#     end
+#   end
+#
+#   def self.new_by_filename(filename)
+#     song = self.new()
+#     song.title = filename.split(" - ")[1]
+#     song
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+# end
 
 class Song
+
   attr_accessor :name, :artist
-  @@all = []
-  def initialize(name, artist=nil)
+
+  def initialize(name)
     @name = name
-    @artist = artist
-    @@all << self
+  end
+
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
-
-  def self.new_by_filename(filename)
-    song = self.new()
-    song.title = filename.split(" - ")[1]
-    song
-  end
-
-  def self.all
-    @@all
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
 end
