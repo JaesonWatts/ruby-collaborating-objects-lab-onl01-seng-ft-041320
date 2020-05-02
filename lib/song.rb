7752992a9b9e0398e892d2a131e4332d939e732e
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:09:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
-# 
-# 
+#
+#
 # class Song
-# 
+#
 #   attr_accessor :name, :artist
-# 
+#
 #   def initialize(name, artist=nil)
 #     @name = name
 #     @artist = artist
-# 
+#
 #   end
-# 
+#
 # end
 
 class Song
