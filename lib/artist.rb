9d551e97d9fef34f632f933dd2ecc8a603c1f690
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 00:41:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,37 +21,77 @@
 #
 # end
 
+# class Artist
+#   attr_accessor :name, :artist
+#   @@all = []
+#   @song_count = 0
+#
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#     @@all << self
+#     #binding.pry
+#   end
+#
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
+#
+#   def add_song_by_name(title)
+#     title = Song.new(title)
+#     add_song(title)
+#   end
+#
+#   def songs
+#     self.all.select {|song| song.artist == self}
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def self.song_count
+#     Song.all.length
+#   end
+# end
+
 class Artist
-  attr_accessor :name, :artist
-  @@all = []
-  @song_count = 0
+
+  attr_accessor :name, :songs
+  @@all =[]
 
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
-    #binding.pry
   end
 
   def add_song(song)
     @songs << song
-    song.artist = self
   end
 
-  def add_song_by_name(title)
-    title = Song.new(title)
-    add_song(title)
-  end
-
-  def songs
-    self.all.select {|song| song.artist == self}
+  def save
+    @@all << self #saves the artist name to the all array
   end
 
   def self.all
-    @@all
+    @@all #displays the all array
   end
 
-  def self.song_count
-    Song.all.length
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
   end
 end
